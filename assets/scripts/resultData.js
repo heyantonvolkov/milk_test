b6e26bfca9262f49b6c23ac44d87032c4f7f072e
Anton <Tonyfirehead@gmail.com>
2020-12-23 18:13:33 +0300

    wip2

--- assets/scripts/resultData.js
+++ assets/scripts/resultData.js
@@ -1,12 +1,12 @@
 const resultData = {
 	holyMilk: {
 		title: 'Невероятно — да ты то самое ангельское, но храброе молочко!',
-		image: 'https://m.media-amazon.com/images/M/MV5BZTAzNzhhMzEtNGQxOS00MmY5LTg4Y2UtNGU3ODE0NzY3N2IzXkEyXkFqcGdeQXVyNzU1NzA3MQ@@._V1_.jpg',
+		image: 'assets/img/milk.jpg',
 		desc: 'Это значит, что на планете Земля скорее всего не существует более доблесного и доброго живого существа, чем ты — невероятно! Аминь! Славься же!'
 	},
 	evilVarenec: {
 		title: 'Чёрт побери! Да ты же самый злобный варенец в истории!',
-		image: 'https://e1.edimdoma.ru/data/recipes/0002/3346/23346-ed4_wide.jpg?1468777698',
+		image: 'assets/img/evil.jpg',
 		desc: 'Это значит, что пока ты читаешь этот текст, скорее всего, ты успел настворить уже целую пачку злобных дел, ограбить детей и слепых стариков! Ужасающе! Славься же!'
 	}
 }
